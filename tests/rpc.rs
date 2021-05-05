17acb42c0af6fd58bb93e06c0fce0034b5ea30ae
Robert Hambrock <roberthambrock@gmail.com>
2021-05-06 00:17:01 +0200

    refactor

--- tests/rpc.rs
+++ tests/rpc.rs
@@ -12,7 +12,13 @@ use microservices::shell::Exec;
 #[test]
 fn spawn_swap() {
     use farcaster_node::{cli::Opts, farcasterd::launch};
-    let mut opts = Opts::parse_from(vec!["swap-cli", "-d", "tests/.farcaster_node", "make"].into_iter());
+    let data_dir = vec!["-d", "tests/.farcaster_node"];
+    let mut opts = Opts::parse_from(
+        vec!["swap-cli"]
+            .into_iter()
+            .chain(data_dir.clone())
+            .chain(vec!["make"]),
+    );
     opts.process();
     info!("opts: {:?}", opts);
     let config: Config = opts.shared.clone().into();
@@ -20,8 +26,7 @@ fn spawn_swap() {
     let mut client = Client::with(config.clone(), config.chain.clone())
         .expect("Error running client");
 
-    let mut farcasterd =
-        launch("../farcasterd", &["-d", "tests/.farcaster_node"]).unwrap();
+    let mut farcasterd = launch("../farcasterd", data_dir.clone()).unwrap();
 
     use std::{thread, time};
     thread::sleep(time::Duration::from_secs_f32(0.5));
@@ -36,11 +41,13 @@ fn spawn_swap() {
         .unwrap_or_else(|err| eprintln!("{} {}", "error:".err(), err.err()));
 
     opts = Opts::parse_from(
-        vec![
-            "swap-cli", "-d", "tests/.farcaster_node", "make", "Testnet", "Bitcoin", "Monero", "101", "100",
-            "10", "30", "20", "Alice", "0.0.0.0", "9376",
-        ]
-        .into_iter(),
+        vec!["swap-cli"]
+            .into_iter()
+            .chain(data_dir.clone())
+            .chain(vec![
+                "make", "Testnet", "Bitcoin", "Monero", "101", "100", "10",
+                "30", "20", "Alice", "0.0.0.0", "9376",
+            ]),
     );
     opts.command
         .exec(&mut client)
@@ -68,5 +75,6 @@ fn spawn_swap() {
         .stdout;
 
     // fails if there are any lingering `farcasterd`s or `peerd`s, so this test is a false-negative if any are running independent of the test
-    assert_eq!("", std::str::from_utf8(&ps_out_grep).unwrap());
+    assert_eq!(ps_out_grep.len(), 0);
+
 }
