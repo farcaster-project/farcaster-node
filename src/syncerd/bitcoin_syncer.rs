b477e6bc765f097d29981d0fe86e29659a85fd62
TheCharlatan <seb.kung@gmail.com>
2021-10-17 00:13:32 +0200

    Syncer: Fix infinite loop in task receiver
    Sleep in the task receiver loop
    
    Also limit the tokio runtime to two worker threads to additionally
    consume fewer system resources

--- src/syncerd/bitcoin_syncer.rs
+++ src/syncerd/bitcoin_syncer.rs
@@ -422,6 +422,7 @@ async fn run_syncerd_task_receiver(
                     // do nothing
                 }
             }
+            tokio::time::sleep(std::time::Duration::from_secs(1)).await;
         }
     });
 }
@@ -615,7 +616,12 @@ impl Synclet for BitcoinSyncer {
         polling: bool,
     ) {
         std::thread::spawn(move || {
-            let rt = Runtime::new().unwrap();
+            use tokio::runtime::Builder;
+            let rt = Builder::new_multi_thread()
+                .worker_threads(2)
+                .enable_all()
+                .build()
+                .unwrap();
             rt.block_on(async {
                 let (event_tx, event_rx): (
                     TokioSender<SyncerdBridgeEvent>,
