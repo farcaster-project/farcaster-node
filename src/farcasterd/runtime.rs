54e15582c5eda5b036105e227defdd0ed2873b40
TheCharlatan <seb.kung@gmail.com>
2022-05-23 13:25:20 +0200

    Remove commented code

--- src/farcasterd/runtime.rs
+++ src/farcasterd/runtime.rs
@@ -1031,8 +1031,6 @@ impl Runtime {
 
             Request::ReadProgress(swapid) => {
                 if let Some(queue) = self.progress.get_mut(&ServiceId::Swap(swapid)) {
-                    // let n = queue.len();
-
                     let mut swap_progress = SwapProgress {
                         failure: None,
                         messages: vec![],
@@ -1052,12 +1050,6 @@ impl Runtime {
                             }
                             _ => unreachable!("not handled here"),
                         };
-                        // let req = if i < n - 1 {
-                        // Request::Progress(x.clone())
-                        // } else {
-                        // Request::Success(OptionDetails(Some(x.clone())))
-                        // };
-                        // report_to.push((Some(source.clone()), req));
                     }
                     senders.send_to(
                         ServiceBus::Ctl,
