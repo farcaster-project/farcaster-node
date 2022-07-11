f6a042f53b29e8c84bd4bad69e92bba25693957c
zkao <zkao@cryp.ee>
2022-07-11 18:34:57 +0200

    increase bob maker sleep before syncer to 5 s

--- src/swapd/runtime.rs
+++ src/swapd/runtime.rs
@@ -1044,7 +1044,7 @@ impl Runtime {
                     Some(remote_commit),
                 );
 
-                std::thread::sleep(Duration::from_secs_f32(2.0));
+                std::thread::sleep(Duration::from_secs_f32(5.0));
                 let btc_fee_task = self.syncer_state.estimate_fee_btc();
                 endpoints.send_to(
                     ServiceBus::Ctl,
