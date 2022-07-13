9f3208609d8391f1096426a6fb75da5030ea7679
zkao <zkao@cryp.ee>
2022-07-13 03:10:18 +0200

    swapd: bug fix, disambiguate pattern, improve logging

--- src/swapd/runtime.rs
+++ src/swapd/runtime.rs
@@ -2035,12 +2035,15 @@ impl Runtime {
                                         request: Request::Protocol(Msg::Reveal(
                                             Reveal::AliceParameters(..)
                                         )),
+                                        dest: ServiceId::Swap(..),
                                         ..
                                     }
                                 )
                             });
-                            if !success {
-                                error!("failed dispatching reveal:aliceparams")
+                            if success {
+                                debug!("successfully dispatched reveal:aliceparams")
+                            } else {
+                                debug!("failed dispatching reveal:aliceparams, maybe sent before")
                             }
                         }
                     }
