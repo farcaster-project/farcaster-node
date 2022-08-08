d95736e529a609badabbfdee59895460684fe8a7
h4sh3d <h4sh3d@protonmail.com>
2022-08-08 17:09:18 +0200

    style: ran cargo fmt

--- tests/monero.rs
+++ tests/monero.rs
@@ -220,7 +220,7 @@ height
 #[ignore]
 async fn monero_syncer_address_test() {
     // TODO enable `lws_address` when the lws wallet starts working with v0.18.0.0
-    for (socket_name, lws_bool) in [("address", false), /*("lws_address", true)*/] {
+    for (socket_name, lws_bool) in [("address", false) /*("lws_address", true)*/] {
         if lws_bool {
             std::env::set_var("RUST_LOG", "farcaster_node=trace,monero-lws=trace");
             setup_logging()
