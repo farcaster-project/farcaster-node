3cd402e3c25b7eea3fbdeac08c43bff487924fc6
Sebastian Kung <seb.kung@gmail.com>
2022-08-15 22:18:31 +0200

    Bitcoin syncer: Refactor subscribe method
    Track subscribed status additionally in syncer state

--- src/syncerd/bitcoin_syncer.rs
+++ src/syncerd/bitcoin_syncer.rs
@@ -125,29 +125,28 @@ impl ElectrumRpc {
     pub fn script_subscribe(
         &mut self,
         address_addendum: BtcAddressAddendum,
-    ) -> Result<Option<AddressNotif>, Error> {
+    ) -> Result<AddressNotif, Error> {
+        debug!("attempting subscribing: {:?}", address_addendum);
+
         if !self.addresses.contains_key(&address_addendum) {
+            debug!("subscribing: {:?}", address_addendum);
             let script_status = self
                 .client
                 .script_subscribe(&address_addendum.address.script_pubkey())?;
             self.addresses
                 .insert(address_addendum.clone(), script_status);
-
             debug!(
                 "registering address {} with script_status {:?}",
                 &address_addendum.address, &script_status
             );
-            if script_status.is_some() {
-                let txs = query_addr_history(&mut self.client, &address_addendum)?;
-                logging(&txs, &address_addendum);
-                let notif = AddressNotif {
-                    address: address_addendum,
-                    txs,
-                };
-                return Ok(Some(notif));
-            }
         }
-        Ok(None)
+        let txs = query_addr_history(&mut self.client, &address_addendum)?;
+        logging(&txs, &address_addendum);
+        let notif = AddressNotif {
+            address: address_addendum,
+            txs,
+        };
+        Ok(notif)
     }
 
     pub fn new_block_check(&mut self) -> Result<Vec<Block>, Error> {
@@ -666,30 +665,33 @@ fn address_polling(
                 let state_guard = state.lock().await;
                 let addresses = state_guard.addresses.clone();
                 drop(state_guard);
-                for (_, address) in addresses.clone() {
+                for (id, address) in addresses.clone() {
                     if let AddressAddendum::Bitcoin(address_addendum) = address.task.addendum {
-                        match rpc.script_subscribe(address_addendum.clone()) {
-                            Ok(Some(addr_notif)) => {
-                                logging(&addr_notif.txs, &address_addendum);
-                                let tx_set = create_set(addr_notif.txs);
-                                let mut state_guard = state.lock().await;
-                                state_guard
-                                    .change_address(
-                                        AddressAddendum::Bitcoin(address_addendum.clone()),
-                                        tx_set,
-                                    )
-                                    .await;
-                                drop(state_guard);
-                            }
-                            // do nothing if we are already subscribed, or there is nothing to change
-                            Ok(None) => {}
-                            // do nothing if we are already subscribed
-                            Err(Error::Syncer(SyncerError::Electrum(
-                                electrum_client::Error::AlreadySubscribed(_),
-                            ))) => {}
-                            Err(e) => {
-                                error!("error in bitcoin address polling: {}", e);
-                                break;
+                        if !address.subscribed {
+                            match rpc.script_subscribe(address_addendum.clone()) {
+                                Ok(notif) => {
+                                    logging(&notif.txs, &address_addendum);
+                                    let tx_set = create_set(notif.txs);
+                                    let mut state_guard = state.lock().await;
+                                    if let Some(address) = state_guard.addresses.get_mut(&id) {
+                                        address.subscribed = true;
+                                    }
+                                    state_guard
+                                        .change_address(
+                                            AddressAddendum::Bitcoin(address_addendum.clone()),
+                                            tx_set,
+                                        )
+                                        .await;
+                                    drop(state_guard);
+                                }
+                                // do nothing if we are already subscribed
+                                Err(Error::Syncer(SyncerError::Electrum(
+                                    electrum_client::Error::AlreadySubscribed(_),
+                                ))) => {}
+                                Err(e) => {
+                                    error!("error in bitcoin address polling: {}", e);
+                                    break;
+                                }
                             }
                         }
                     }
