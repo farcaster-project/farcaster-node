16e662b7b5557b9b5ccaf236d0bf151ce4384f49
zkao <zkao@cryp.ee>
2022-08-16 14:15:42 +0200

    swap: bug fix, Abort both RPC and Ctl message

--- src/swapd/runtime.rs
+++ src/swapd/runtime.rs
@@ -634,6 +634,7 @@ impl Runtime {
                 ServiceId::Farcasterd
                 | ServiceId::Wallet
                 | ServiceId::Database
+                | ServiceId::Swap(_) // for recursing
             ) if white_list_ctl_msgs(r) => {
                 self.process(endpoints, source.clone(), request.clone())?;
                 return Ok(())
@@ -1007,7 +1008,7 @@ impl Runtime {
                                         msg,
                                     )?;
                                     // FIXME: syncer shall not have permission to AbortSwap, replace source by identity?
-                                    self.handle_ctl(endpoints, source, Request::AbortSwap)?;
+                                    self.handle_ctl(endpoints, self.identity(), Request::AbortSwap)?;
                                     return Ok(());
                                 }
 
@@ -1746,6 +1747,7 @@ fn white_list_ctl_msgs(request: &Request) -> bool {
         Request::SweepBitcoinAddress(_) => true,
         Request::PeerdReconnected => true,
         Request::Checkpoint(_) => true,
+        Request::AbortSwap => true,
         _ => false,
     }
 }
