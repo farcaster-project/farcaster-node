d0a459c989d7dc84867e49dae9d7880cdd94e185
Leonardo Nerone <leofnerone@proton.me>
2022-08-22 21:20:45 -0300

    docs(tests): add explanation for allowing dead code

--- tests/utils/mod.rs
+++ tests/utils/mod.rs
@@ -1,5 +1,11 @@
 //! Various utils to help build functional tests.
 
+// dead code is allowed because there is a possible error in Cargo that warns
+// about dead functions even if they are used;
+// see https://github.com/rust-lang/rust/issues/46379
+// a possible workaround is https://github.com/rust-lang/rust/issues/46379#issuecomment-487421236
+// but as described in https://github.com/rust-lang/rust/issues/46379#issuecomment-487544472,
+// such solution hides code that is truly dead.
 #![allow(dead_code)]
 
 pub mod assert;
@@ -12,7 +18,7 @@ pub fn setup_logging() {
     // set default level to info, debug for node
     // info level is for bitcoin and monero that correspond to `tests/{bitcoin.rs,monero.rs}`
     let env = env_logger::Env::new().default_filter_or("info,farcaster_node=debug");
-    // try init the logger, this fails if logger has been already initialized
+    // try init the logger; this fails if logger has been already initialized
     // and it happens when multiple tests are called as the (test)binary is not restarted
     let _ = env_logger::from_env(env).is_test(true).try_init();
 }
