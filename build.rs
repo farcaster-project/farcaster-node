62841dc17ad4dd6bfc11ff530ea07f2c54cece2e
h4sh3d <h4sh3d@protonmail.com>
2022-08-24 12:41:10 +0200

    refactor: move shell completion at runtime and for swap-cli only

--- build.rs
+++ build.rs
@@ -12,45 +12,7 @@
 // along with this software.
 // If not, see <https://opensource.org/licenses/MIT>.
 
-#[macro_use]
-extern crate amplify_derive;
-#[macro_use]
-extern crate clap;
-
-use clap::IntoApp;
-use clap_complete::generate_to;
-use clap_complete::shells::*;
-
-pub mod opts {
-    include!("src/opts.rs");
-}
-
-pub mod cli {
-    include!("src/cli/opts.rs");
-}
-pub mod farcasterd {
-    include!("src/farcasterd/opts.rs");
-}
-pub mod peerd {
-    include!("src/peerd/opts.rs");
-}
-pub mod swapd {
-    include!("src/swapd/opts.rs");
-}
-pub mod syncerd {
-    include!("src/syncerd/opts.rs");
-}
-pub mod walletd {
-    include!("src/walletd/opts.rs");
-}
-pub mod databased {
-    include!("src/databased/opts.rs");
-}
-pub mod grpcd {
-    include!("src/grpcd/opts.rs");
-}
-
-fn main() -> Result<(), std::io::Error> {
+fn main() {
     let proto_file = "./src/grpcd/proto/farcaster.proto";
 
     tonic_build::configure()
@@ -59,27 +21,4 @@ fn main() -> Result<(), std::io::Error> {
         .unwrap_or_else(|e| panic!("protobuf compile error: {}", e));
 
     println!("cargo:rerun-if-changed={}", proto_file);
-
-    let outdir = "./shell";
-
-    for app in [
-        farcasterd::Opts::command(),
-        peerd::Opts::command(),
-        swapd::Opts::command(),
-        cli::Opts::command(),
-        walletd::Opts::command(),
-        syncerd::Opts::command(),
-        databased::Opts::command(),
-        grpcd::Opts::command(),
-    ]
-    .iter_mut()
-    {
-        let name = app.get_name().to_string();
-        generate_to(Bash, app, &name, &outdir)?;
-        generate_to(PowerShell, app, &name, &outdir)?;
-        generate_to(Zsh, app, &name, &outdir)?;
-        generate_to(Fish, app, &name, &outdir)?;
-    }
-
-    Ok(())
 }
