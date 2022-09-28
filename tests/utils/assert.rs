3036894c5bcf27ad1b6cd27ce610332c895a935b
h4sh3d <h4sh3d@protonmail.com>
2022-09-28 09:43:49 +0200

    fix: BusMsg renaming in tests

--- tests/utils/assert.rs
+++ tests/utils/assert.rs
@@ -1,14 +1,14 @@
 //! Serie of helper assertion functions.
 
 use farcaster_node::bus::sync::{BridgeEvent, SyncMsg};
-use farcaster_node::bus::Request;
+use farcaster_node::bus::BusMsg;
 use farcaster_node::syncerd::types::Event;
 use farcaster_node::syncerd::TaskId;
 use farcaster_node::syncerd::{FeeEstimation, FeeEstimations};
 
-pub fn address_transaction(request: Request, expected_amount: u64, possible_txids: Vec<Vec<u8>>) {
+pub fn address_transaction(request: BusMsg, expected_amount: u64, possible_txids: Vec<Vec<u8>>) {
     match request {
-        Request::Sync(SyncMsg::BridgeEvent(event)) => match event.event {
+        BusMsg::Sync(SyncMsg::BridgeEvent(event)) => match event.event {
             Event::AddressTransaction(address_transaction) => {
                 assert_eq!(address_transaction.amount, expected_amount);
                 assert!(possible_txids.contains(&address_transaction.hash));
@@ -19,9 +19,9 @@ pub fn address_transaction(request: Request, expected_amount: u64, possible_txid
     }
 }
 
-pub fn sweep_success(request: Request, id: TaskId) {
+pub fn sweep_success(request: BusMsg, id: TaskId) {
     match request {
-        Request::Sync(SyncMsg::BridgeEvent(event)) => match event.event {
+        BusMsg::Sync(SyncMsg::BridgeEvent(event)) => match event.event {
             Event::SweepSuccess(sweep_success) => {
                 assert_eq!(sweep_success.id, id);
             }
@@ -31,9 +31,9 @@ pub fn sweep_success(request: Request, id: TaskId) {
     }
 }
 
-pub fn received_height_changed(request: Request, expected_height: u64) {
+pub fn received_height_changed(request: BusMsg, expected_height: u64) {
     match request {
-        Request::Sync(SyncMsg::BridgeEvent(event)) => match event.event {
+        BusMsg::Sync(SyncMsg::BridgeEvent(event)) => match event.event {
             Event::HeightChanged(height_changed) => {
                 assert_eq!(height_changed.height, expected_height);
             }
@@ -48,12 +48,12 @@ pub fn received_height_changed(request: Request, expected_height: u64) {
 }
 
 pub fn transaction_confirmations(
-    request: Request,
+    request: BusMsg,
     expected_confirmations: Option<u32>,
     expected_block_hash: Vec<u8>,
 ) {
     match request {
-        Request::Sync(SyncMsg::BridgeEvent(event)) => match event.event {
+        BusMsg::Sync(SyncMsg::BridgeEvent(event)) => match event.event {
             Event::TransactionConfirmations(transaction_confirmations) => {
                 assert_eq!(
                     transaction_confirmations.confirmations,
@@ -67,9 +67,9 @@ pub fn transaction_confirmations(
     }
 }
 
-pub fn task_aborted(request: Request, expected_error: Option<String>, mut expected_id: Vec<u32>) {
+pub fn task_aborted(request: BusMsg, expected_error: Option<String>, mut expected_id: Vec<u32>) {
     match request {
-        Request::Sync(SyncMsg::BridgeEvent(event)) => match event.event {
+        BusMsg::Sync(SyncMsg::BridgeEvent(event)) => match event.event {
             Event::TaskAborted(mut task_aborted) => {
                 assert_eq!(
                     &task_aborted.id.sort_unstable(),
@@ -87,9 +87,9 @@ pub fn task_aborted(request: Request, expected_error: Option<String>, mut expect
     }
 }
 
-pub fn transaction_broadcasted(request: Request, has_error: bool, error_msg: Option<String>) {
+pub fn transaction_broadcasted(request: BusMsg, has_error: bool, error_msg: Option<String>) {
     match request {
-        Request::Sync(SyncMsg::BridgeEvent(event)) => match event.event {
+        BusMsg::Sync(SyncMsg::BridgeEvent(event)) => match event.event {
             Event::TransactionBroadcasted(transaction_broadcasted) => {
                 if has_error {
                     assert!(transaction_broadcasted.error.is_some());
@@ -110,9 +110,9 @@ pub fn transaction_broadcasted(request: Request, has_error: bool, error_msg: Opt
     }
 }
 
-pub fn transaction_received(request: Request, expected_txid: bitcoin::Txid) {
+pub fn transaction_received(request: BusMsg, expected_txid: bitcoin::Txid) {
     match request {
-        Request::Sync(SyncMsg::BridgeEvent(event)) => match event.event {
+        BusMsg::Sync(SyncMsg::BridgeEvent(event)) => match event.event {
             Event::TransactionRetrieved(transaction) => {
                 assert_eq!(transaction.tx.unwrap().txid(), expected_txid);
             }
@@ -126,9 +126,9 @@ pub fn transaction_received(request: Request, expected_txid: bitcoin::Txid) {
     }
 }
 
-pub fn fee_estimation_received(request: Request) {
+pub fn fee_estimation_received(request: BusMsg) {
     match request {
-        Request::Sync(SyncMsg::BridgeEvent(BridgeEvent {
+        BusMsg::Sync(SyncMsg::BridgeEvent(BridgeEvent {
             event:
                 Event::FeeEstimation(FeeEstimation {
                     fee_estimations:
