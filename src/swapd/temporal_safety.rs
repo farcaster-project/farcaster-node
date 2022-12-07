981c0d1d93986f7584bd4b32efbbe41e5ae1854e
h4sh3d <h4sh3d@protonmail.com>
2022-12-07 17:15:42 +0100

    style: format impl block

--- src/swapd/temporal_safety.rs
+++ src/swapd/temporal_safety.rs
@@ -33,10 +33,9 @@ pub struct TemporalSafety {
 }
 
 impl TemporalSafety {
-    /// check if temporal params are in correct order
+    /// Validate if temporal parameters are coherent
     pub fn valid_params(&self) -> Result<(), Error> {
         let btc_finality = self.btc_finality_thr;
-        // let xmr_finality = self.xmr_finality_thr;
         let cancel = self.cancel_timelock;
         let punish = self.punish_timelock;
         let race = self.race_thr;
@@ -45,16 +44,16 @@ impl TemporalSafety {
             && btc_finality < race
             && punish > race
             && cancel > race
-        // && btc_finality < xmr_finality
         {
             Ok(())
         } else {
             Err(Error::Farcaster(s!(
-                "unsafe and invalid temporal parameters, timelocks, race and tx finality params"
+                "Unsafe or invalid temporal parameters between: timelocks, finality and safety"
             )))
         }
     }
-    /// returns whether tx is final given the finality threshold set for the chain
+
+    /// Returns whether tx is final given the finality threshold set for the chain
     pub fn final_tx(&self, confs: u32, blockchain: Blockchain) -> bool {
         let finality_thr = match blockchain {
             Blockchain::Bitcoin => self.btc_finality_thr,
@@ -62,40 +61,49 @@ impl TemporalSafety {
         };
         confs >= finality_thr
     }
-    /// lock must be final, cancel cannot be raced, add + 1 to offset initial lock confirmation
+
+    /// Lock must be final, cancel cannot be raced, add + 1 to offset initial lock confirmation
     pub fn stop_funding_before_cancel(&self, lock_confirmations: u32) -> bool {
         self.final_tx(lock_confirmations, Blockchain::Bitcoin)
             && lock_confirmations > (self.cancel_timelock - self.race_thr + 1)
     }
-    // blocks remaining until funding will be stopped for safety, because it is too close to cancel. Adds the same +1 offset as in stop_funding_before_cancel
+
+    // Blocks remaining until funding will be stopped for safety, because it is too close to
+    // cancel. Adds the same +1 offset as in stop_funding_before_cancel
     pub fn blocks_until_stop_funding(&self, lock_confirmations: u32) -> i64 {
         self.cancel_timelock as i64 - (self.race_thr as i64 + 1 + lock_confirmations as i64)
     }
-    /// lock must be final, valid after lock_minedblock + cancel_timelock
+
+    /// Lock must be final, valid after lock_minedblock + cancel_timelock
     pub fn valid_cancel(&self, lock_confirmations: u32) -> bool {
         self.final_tx(lock_confirmations, Blockchain::Bitcoin)
             && lock_confirmations >= self.cancel_timelock
     }
-    /// blocks remaining until cancel, copies logic from valid_cancel
+
+    /// Blocks remaining until cancel, copies logic from valid_cancel
     pub fn blocks_until_cancel(&self, lock_confirmations: u32) -> i64 {
         self.cancel_timelock as i64 - lock_confirmations as i64
     }
-    /// lock must be final, but buy shall not be raced with cancel
+
+    /// Lock must be final, but buy shall not be raced with cancel
     pub fn safe_buy(&self, lock_confirmations: u32) -> bool {
         self.final_tx(lock_confirmations, Blockchain::Bitcoin)
             && lock_confirmations <= (self.cancel_timelock - self.race_thr)
     }
-    /// cancel must be final, but refund shall not be raced with punish
+
+    /// Cancel must be final, but refund shall not be raced with punish
     pub fn safe_refund(&self, cancel_confirmations: u32) -> bool {
         self.final_tx(cancel_confirmations, Blockchain::Bitcoin)
             && cancel_confirmations <= (self.punish_timelock - self.race_thr)
     }
-    /// cancel must be final, valid after cancel_confirmations > punish_timelock
+
+    /// Cancel must be final, valid after cancel_confirmations > punish_timelock
     pub fn valid_punish(&self, cancel_confirmations: u32) -> bool {
         self.final_tx(cancel_confirmations, Blockchain::Bitcoin)
             && cancel_confirmations >= self.punish_timelock
     }
-    /// blocks remaning until punish, copies logic from valid_punish
+
+    /// Blocks remaning until punish, copies logic from valid_punish
     pub fn blocks_until_punish_after_cancel(&self, cancel_confirmations: u32) -> i64 {
         self.punish_timelock as i64 - cancel_confirmations as i64
     }
