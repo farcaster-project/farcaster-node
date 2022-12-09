2c3520c364d0ac69127d8f796d8260443926311b
TheCharlatan <seb.kung@gmail.com>
2022-12-09 10:26:46 +0100

    Tests: Wait a bit before killing daemons in checkpoint tests to ensure the checkpoints are set

--- tests/swap.rs
+++ tests/swap.rs
@@ -1185,6 +1185,9 @@ async fn run_restore_checkpoint_bob_pre_buy_alice_pre_lock(
     info!("waiting for the bitcoin funding info to clear");
     retry_until_funding_info_cleared(swap_id, cli_bob_needs_funding_args.clone()).await;
 
+    // wait a bit to ensure the checkpoints are set
+    tokio::time::sleep(time::Duration::from_secs(2)).await;
+
     // kill all the daemons and start them again
     kill_all();
     let _ = launch_farcasterd_pair().await;
