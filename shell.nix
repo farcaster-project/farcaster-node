a016ce42fa0dfa9209ddbc184b65f9627d6ed722
Robert Hambrock <roberthambrock@gmail.com>
2024-08-11 00:08:57 +0200

    update shell.nix
    The `shell.nix` was out of date with the latest rust toolchain required
    for building `main`. This now uses (close to) latest `nixpkgs` revision
    compatible without replacing the `zmq2` dependency.

--- shell.nix
+++ shell.nix
@@ -1,19 +1,16 @@
 let
-  pkgs = import
-    (builtins.fetchGit {
-      name = "nixos-release-22.05";
-      url = "https://github.com/nixos/nixpkgs/";
-      ref = "refs/heads/release-22.05";
-      rev = "6ddd2d34e3701339eb01bbf1259b258adcc6156c";
-    })
-    {
-      overlays = [
-        (import (builtins.fetchTarball
-          "https://github.com/oxalica/rust-overlay/archive/master.tar.gz"))
-      ];
-    };
-in
-pkgs.mkShell {
+  pkgs = import (builtins.fetchGit {
+    name = "nixos-release-24.05";
+    url = "https://github.com/nixos/nixpkgs/";
+    ref = "refs/heads/release-24.05";
+    rev = "9957cd48326fe8dbd52fdc50dd2502307f188b0d";
+  }) {
+    overlays = [
+      (import (builtins.fetchTarball
+        "https://github.com/oxalica/rust-overlay/archive/master.tar.gz"))
+    ];
+  };
+in pkgs.mkShell {
   buildInputs = with pkgs; [
     cargo
     cargo-watch
